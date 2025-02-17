47414013f61ddc30f6263c894f15e8f3bfeccb04
officialYash <38553961+officialYash@users.noreply.github.com>
2025-02-17 09:41:08 +0530

    create routes and middleware of user

---
+++ Backend/middlewares/isAuthentication.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+const isAuthentication = async (req, res, next) => {
+try {
+    const token = req.cookies.token;
+    if (!token){ return res.status(401).json({ message: "Unauthorized", success: false });
+}
+const decode = jwt.verify(token, process.env.JWT_SECRET);
+if (!decode) {
+    return res.status(401).json({ message: "Unauthorized", success: false });
+}
+req.id = decode.userId;
+next();
+}
+catch (error) {
+    console.log(error);
+}
+}
+export default isAuthentication;
