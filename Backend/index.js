834b3e239ae5eb850968ec7057578c37eaec1c2a
officialYash <38553961+officialYash@users.noreply.github.com>
2025-02-20 18:35:41 +0530

    create application controller and  routes page

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,8 @@ import connectDB from './utils/db.js';
 import userRoute from './routes/user.routes.js';
 import companyRoute from './routes/company.routes.js'; 
 import jobRoute from './routes/job.routes.js';
+import applicationRoute from './routes/application.routes.js';
+
 
 dotenv.config();
 const app = express();
@@ -25,6 +27,7 @@ const PORT = process.env.PORT || 3000;
 app.use('/api/v1/user',userRoute);
 app.use('/api/v1/company',companyRoute);
 app.use('/api/v1/job',jobRoute);
+app.use('/api/v1/application',applicationRoute);
 
 app.listen(PORT,()=>{
     connectDB();
