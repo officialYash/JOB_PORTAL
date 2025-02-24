5ebef49aee56c3911ecd4cac4282adaa1ee29486
officialYash <38553961+officialYash@users.noreply.github.com>
2025-02-24 17:28:12 +0530

    Create create a page of login and signup

--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -1,9 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Button } from "../ui/button";
 import { Avatar, AvatarImage } from "../ui/avatar";
+import { LogOut, User2 } from "lucide-react";
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16 ">
@@ -18,33 +21,47 @@ const Navbar = () => {
             <li>Jobs</li>
             <li>Browse</li>
           </ul>
-          <Popover>
-            <PopoverTrigger>
-              <Avatar className="cursor-pointer">
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-              </Avatar>
-            </PopoverTrigger>
-            <PopoverContent className='w-80'>
-            <div className="flex gap-4 space-y-2">
-            <Avatar className="cursor-pointer">
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-              </Avatar>
-              <div>
-              <h4 className="font-medium"> Official Yash</h4>
-              <p className="text-sm text-muted-foreground">jsiahsi</p>
-              </div>
-            </div>
+          {!user ? (
             <div>
-                <Button variant="link">View Profile</Button>
+              <Button variant="outline">Login</Button>
+              <Button className='bg-[#6A38C2] hover:bg-[#5b30a6]'> SignUp</Button>
             </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                  />
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent className="w-80">
+                <div className="flex gap-4 space-y-2">
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium"> Official Yash</h4>
+                    <p className="text-sm text-muted-foreground">jsiahsi</p>
+                  </div>
+                </div>
+                <div className="flex flex-col my-2 text-gray-600">
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <User2 />
+                    <Button variant="link">View Profile</Button>
+                  </div>
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <LogOut />
+                    <Button variant="link">Log Out</Button>
+                  </div>
+                </div>
               </PopoverContent>
-          </Popover>
+            </Popover>
+          )}
         </div>
       </div>
     </div>
